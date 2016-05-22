18c235507e87ff0214be570dbcd4074ee8bb89e8
the incredible hulch <jhulcher@gmail.com>
2016-05-22 15:26:57 -0700

    Alter webpack config js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /node_modules|bower_components/,
         loader: 'babel',
         query: {
           presets: ['react']
